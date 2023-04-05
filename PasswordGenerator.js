4a7f69f0b71d5433d881834a21fae92c9ae540c3
Aram Valcourt <115132116+AramCodes@users.noreply.github.com>
2023-04-05 17:32:39 -0400

    Add files via upload

--- PasswordGenerator.js
+++ PasswordGenerator.js
@@ -1,10 +1,11 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", 
 "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
-let passwordLength = 15
+let passwordLength = 16
 let generateButton = document.getElementById("generate-btn")
 let passwordField1 = document.getElementById("pfeild1")
 let passwordField2 = document.getElementById("pfeild2")
+
 
 generateButton.addEventListener("click", generateRandomPassword)     // trying to add an event listener to generate button
 
@@ -15,19 +16,20 @@ function getRandomCharacter() {
 }
 
 function generateRandomPassword() {
-    let randomPassword = ""                            //trying to make generated 15 length password go into pfield 1
+    let randomPassword = ""                            //trying to make generated 16 length password go into pfield 1
     for (let i = 0; i < passwordLength; i++) {
         randomPassword += getRandomCharacter()         
     }
     passwordField1.textContent = randomPassword
-    
+     
 }
 
 function generateRandomPassword2() {
-    let randomPassword = ""                            //trying to make generated 15 length password go into pfield 2
+    let randomPassword = ""                            //trying to make generated 16 length password go into pfield 2
     for (let i = 0; i < passwordLength; i++) {
         randomPassword += getRandomCharacter()         
     }
     passwordField2.textContent = randomPassword
     
 }
+
